ff86956db4977016a5ef961002cfb28910885323
Mouhamadou Diamanka <diamanka1998@gmail.com>
2023-08-24 10:32:26 +0000

    first

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const bodyParse = require("body-parser");
+const path = require("path");
+const cors = require("cors");
+const cron = require("node-cron");
+const expireCarte = require("./Jobs/expirationCarteJob");
+
+const routesUtilisateur = require("./routes/userRoute");
+
+
+app.use(cors());
+app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization,multipart/form-data"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+app.use(bodyParse.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+
+app.use("/user", routesUtilisateur);
+app.use("/poste", routesPoste);
+
+module.exports = app;
